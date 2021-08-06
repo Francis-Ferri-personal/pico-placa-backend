5138421f48ad853bc2af5d2ec338c15eb59cc3a3
Francis Ferri <francisferrir@gmail.com>
2021-08-06 12:44:29 -0500

    Exceptions control

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,14 +1,25 @@
 import { Router, Request, Response } from "express";
+import PicoPlacaValidator from "../models/picoPlacaValidator";
 
 const router = Router();
 
 router.post("/pico-placa", (req: Request, res: Response) => {
 	const { plateNumber, date, time } = req.body;
 
+	const picoPlacaValidator = new PicoPlacaValidator(plateNumber, date, time);
+
+	const allowed = picoPlacaValidator.validatePicoPlaca();
+	if (!allowed) {
+		res.status(200).json({
+			allowed,
+			msg: `Not allowed to drive - date: ${date} time: ${time}`
+		});
+		return;
+	}
+
 	res.status(200).json({
-		ok: true,
-		allowed: true,
-		msg: "Test successful"
+		allowed,
+		msg: `Allowed to drive - date: ${date} time: ${time}`
 		// can be on the road?
 	});
 });
